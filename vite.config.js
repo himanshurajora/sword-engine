3dca5da1b43e3017d06dd449f7d41a679db77947
Himanshu Jangid <iamahalofan@gmail.com>
2022-01-01 18:17:43 +0530

    deployed

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        game1: resolve(__dirname, 'samples/games/FlappyBird/index.html'),
+        game2: resolve(__dirname, 'samples/games/Snake/index.html'),
+        game3: resolve(__dirname, 'samples/games/SpaceWar/index.html'),
+        simulation1: resolve(__dirname, 'samples/simulations/Raining/index.html'),
+        simulation2: resolve(__dirname, 'samples/simulations/BeziereCurves/index.html'),
+        simulation3: resolve(__dirname, 'samples/simulations/SolorSystem/index.html'),
+        dsaSimulation1: resolve(__dirname, 'samples/dsa-simulation/knapsack-DP/index.html')
+    
+      }
+    }
+  }
+})
